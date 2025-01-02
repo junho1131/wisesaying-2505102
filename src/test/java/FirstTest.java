e8eb06299056ddff9d358a7ac903340a63c8884b
junho1131 <45115780+junho1131@users.noreply.github.com>
2025-01-02 15:30:45 +0900

    2st commit

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,11 +1,23 @@
+import org.example.TestApp;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FirstTest {
 
+
     @Test
-    void t1(){
-        assertThat(1).isEqualTo(1);
+    void t1() {
+        int rst = 1;
+        assertThat(rst).isEqualTo(1);
+    }
+
+    @Test
+    void t2() {
+        TestApp app = new TestApp();
+        app.run();
+
+        // aaa가 출력되는가?
+        // assertThat(result).isEqualTo("aaa");
     }
 }

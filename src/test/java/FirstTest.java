5d01c0d28d4dfd668f2e65540320e5f6f48581fa
junho1131 <45115780+junho1131@users.noreply.github.com>
2025-01-02 15:16:15 +0900

    first

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FirstTest {
+
+    @Test
+    void t1(){
+        assertThat(1).isEqualTo(1);
+    }
+}
